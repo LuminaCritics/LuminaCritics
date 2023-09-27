78b46f3f9c497183290dde91cb30038d83bfb59d
MDyeimison <marcosdyeimison@gmail.com>
2023-09-27 19:55:48 -0300

    feature: criando email

--- backend/critics/views.py
+++ backend/critics/views.py
@@ -1,3 +1,11 @@
+from django.conf import settings
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
+subject = 'Test Email'
+message = 'This is a test email sent from my Django app.'
+from_email = settings.EMAIL_HOST_USER
+recipient_list = ['testeemail@gmail.com']
+
+send_mail(subject, message, from_email, recipient_list, fail_silently=False)
--- backend/luminacritics/settings.py
+++ backend/luminacritics/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_rest_passwordreset',
+    'critics'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#EMAIL
+#posteriormente colocar o EMAIL_HOST_USER e EMAIL_HOST_PASSWORD no arquivo env
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'luminacritics@gmail.com'
+EMAIL_HOST_PASSWORD = 'dvia icby vfns yuvl'
