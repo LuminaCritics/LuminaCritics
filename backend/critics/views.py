39c28da70d58905f9f46aa82f7adc1b275a15b27
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-09-27 12:13:54 -0300

    feat: adicionando obtencao de token e autenticacao

--- backend/critics/views.py
+++ backend/critics/views.py
@@ -2,16 +2,25 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.generics import RetrieveUpdateAPIView, DestroyAPIView
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.permissions import IsAuthenticated
+
 from .models import Usuario
 from .serializers import UsuarioSerializer
 
 class UsuarioListView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
         usuarios = Usuario.objects.all()
         serializer = UsuarioSerializer(usuarios, many=True)
         return Response(serializer.data)
 
 class UsuarioCreateView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def post(self, request):
         serializer = UsuarioSerializer(data=request.data)
         if serializer.is_valid():
@@ -20,9 +29,20 @@ class UsuarioCreateView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class UsuarioUpdateView(RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
 
 class UsuarioDeleteView(DestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
+
+User = get_user_model()
+
+class UsuarioLoginView(TokenObtainPairView):
+    pass
+
+class UsuarioLogoutView(generics.CreateAPIView):
+    authentication_classes = ()
+    permission_classes = ()
