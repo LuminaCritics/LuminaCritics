3d703117ded0d3aa4af72124c8959e49ac7a795f
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-09-27 11:42:19 -0300

    feat: criando as views do CRUD

--- backend/critics/views.py
+++ backend/critics/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.generics import RetrieveUpdateAPIView, DestroyAPIView
+from .models import Usuario
+from .serializers import UsuarioSerializer
 
-# Create your views here.
+class UsuarioListView(APIView):
+    def get(self, request):
+        usuarios = Usuario.objects.all()
+        serializer = UsuarioSerializer(usuarios, many=True)
+        return Response(serializer.data)
+
+class UsuarioCreateView(APIView):
+    def post(self, request):
+        serializer = UsuarioSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UsuarioUpdateView(RetrieveUpdateAPIView):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+class UsuarioDeleteView(DestroyAPIView):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
