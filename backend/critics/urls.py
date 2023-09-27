4ad738062bb6e1e50e8ffa59d01990b0d37ecf83
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-09-27 12:14:44 -0300

    feat: adicionando urls de login/logout/refresh

--- backend/critics/urls.py
+++ backend/critics/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from . import views
 
 urlpatterns = [
@@ -6,4 +8,7 @@ urlpatterns = [
     path('usuarios/criar/', views.UsuarioCreateView.as_view(), name='usuario-create'),
     path('usuarios/<int:pk>/', views.UsuarioUpdateView.as_view(), name='usuario-update'),
     path('usuarios/<int:pk>/remover/', views.UsuarioDeleteView.as_view(), name='usuario-delete'),
+    path('login/', views.UsuarioLoginView.as_view(), name='usuario-login'),  # Endpoint de login JWT
+    path('logout/', views.UsuarioLogoutView.as_view(), name='usuario-logout'),  # Endpoint de logout (opcional)
+    path('token-refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 ]
