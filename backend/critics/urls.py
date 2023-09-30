40d9c2faa81871a3d4ef55799715b5a4586f7296
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-09-30 18:08:18 -0300

    refact: configurando as novas rotas

--- backend/critics/urls.py
+++ backend/critics/urls.py
@@ -1,17 +1,25 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-from . import views
+from .views import(
+    register_user,
+    get_users,
+    update_user,
+    delete_user,
+)
 
 schema_view = get_schema_view(
     openapi.Info(
         title="Lumina Critics",
         default_version='v1',
         description="Recomendações de filmes",
-        #terms_of_service="https://www.suaapi.com/terms/",
         contact=openapi.Contact(email="luminacritics@gmail.com"),
         license=openapi.License(name="MIT License"),
     ),
@@ -20,13 +28,12 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('usuarios/', views.UsuarioListView.as_view(), name='usuario-list'),
-    path('usuarios/criar/', views.UsuarioCreateView.as_view(), name='usuario-create'),
-    path('usuarios/<int:pk>/', views.UsuarioUpdateView.as_view(), name='usuario-update'),
-    path('usuarios/<int:pk>/remover/', views.UsuarioDeleteView.as_view(), name='usuario-delete'),
-    path('login/', views.UsuarioLoginView.as_view(), name='usuario-login'),  # Endpoint de login JWT
-    path('logout/', views.UsuarioLogoutView.as_view(), name='usuario-logout'),  # Endpoint de logout (opcional)
-    path('token-refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('usuarios/', get_users, name='listar-usuarios'),
+    path('usuarios/criar/', register_user, name='criar-usuarios'),
+    path('usuarios/<int:user_id>/', update_user, name='atualizar-usuario'),
+    path('usuarios/<int:user_id>/remover/', delete_user, name='deletar-usuario'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
