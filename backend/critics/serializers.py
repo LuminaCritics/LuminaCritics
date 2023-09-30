983d58063302747afe4ea8db40627f090b5fd3e4
João Gabriel <joao.alves1@mail.uft.edu.br>
2023-09-30 16:58:43 -0300

    revertendo erro de pull request

--- backend/critics/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Usuario
-
-class UsuarioSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Usuario
-        fields = ('id', 'primeiro_nome', 'sobrenome', 'email')
