def156cdeb481e4a6d70f78e56c9c4ccc8389d85
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-11-10 00:47:11 -0300

    feat: criando middleware para tokens jwt

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -1,4 +1,6 @@
-const bcryptjs = require("bcryptjs");
+require('dotenv').config();
+const jwt = require("jsonwebtoken");
+const bcryptjs = require("bcrypt");
 
 async function hash(password) {
   return await bcryptjs.hash(password, 14);
@@ -8,8 +10,21 @@ async function compare(providedPassword, storedPassword) {
   return await bcryptjs.compare(providedPassword, storedPassword);
 }
 
+function checkToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-module.exports = Object.freeze({
-  hash,
-  compare,
-});
+  if (!token) return res.status(401).json({ message: "Acesso negado!" });
+
+  try {
+    const secret = process.env.SECRET;
+
+    jwt.verify(token, secret);
+
+    next();
+  } catch (err) {
+    res.status(400).json({ message: "O token é inválido!" });
+  }
+}
+
+module.exports = checkToken, hash, compare;
