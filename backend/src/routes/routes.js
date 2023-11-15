397a09670c6ea5fc20c49dc1218254d8d811b0db
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-11-15 17:44:09 -0300

    feat: criando validação para create e update

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const userController = require("../controllers/userController");
 const checkToken = require("../middlewares/auth");
+const { insertValidation, updateValidation, loginValidation } = require("../middlewares/validationMiddleware")
 const router = express.Router();
 
 router.route("/users").get(checkToken, userController.getAll);
-router.route("/users/create").post(userController.create);
-router.route("/users/login").post(userController.login);
+router.route("/users/create").post(insertValidation, userController.create);
+router.route("/users/login").post(loginValidation, userController.login);
 router
   .route("/users/:id")
-  .put(checkToken, userController.update)
+  .put(updateValidation, checkToken, userController.update)
   .get(checkToken, userController.findById)
   .delete(checkToken, userController.delete);
 
