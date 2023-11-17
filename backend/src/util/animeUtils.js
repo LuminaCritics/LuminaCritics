095f1196f0b7f5308f669f229ded0e1144a23b59
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-11-16 23:13:02 -0300

    feat: função para filtrar os dados vindos da api

---
+++ backend/src/util/animeUtils.js
@@ -0,0 +1,26 @@
+function extractDesiredFields(data) {
+    const {
+      id,
+      title,
+      url,
+      genres,
+      totalEpisodes,
+      image,
+      releaseDate,
+      description,
+    } = data;
+  
+    return {
+      id,
+      title,
+      url,
+      genres,
+      totalEpisodes,
+      image,
+      releaseDate,
+      description,
+    };
+  }
+  
+  module.exports = { extractDesiredFields };
+  
