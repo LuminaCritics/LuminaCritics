e2b3ece9f3415eee925e18d832504a10ee140c59
Emanuel Montenegro <60626167+emanuelcatao@users.noreply.github.com>
2023-12-06 18:01:30 -0300

    Update mainModel.js

--- backend/src/models/mainModel.js
+++ backend/src/models/mainModel.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: "mysql",
+  dialect: "postgres",
   port: process.env.DB_PORT,
   pool: {
     max: 10,
