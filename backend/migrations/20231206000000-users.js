55737be109de597cf0085213eae79ee94a6b64f8
Emanuel Montenegro <60626167+emanuelcatao@users.noreply.github.com>
2023-12-06 22:28:34 -0300

    Update 20231206000000-users.js

--- backend/migrations/20231206000000-users.js
+++ backend/migrations/20231206000000-users.js
@@ -5,6 +5,11 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.createTable
     ('users',{
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
       name: {
         type: Sequelize.STRING,
       },
