3a2ee6ba4955beaef546f64de6382e2786284b74
icarompo <icaromesquitaponce@outlook.com>
2023-12-08 02:18:25 -0300

    feat: cria home, header e footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer className="footer footer-center bottom-0 p-10 bg-base-200 text-base-content rounded">
+      <nav className="grid grid-flow-col gap-4">
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Jobs</a>
+        <a className="link link-hover">Press kit</a>
+      </nav>
+      <nav>
+      </nav>
+      <aside>
+        <p>Copyright © 2023 - All right reserved by Luminacritics Ltd</p>
+      </aside>
+    </footer>
+  );
+}
+
+export default Footer;
