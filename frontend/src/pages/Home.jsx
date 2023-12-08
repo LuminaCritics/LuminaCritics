b9b9f1f5448d0e6022396e686cc5477a04bd2238
icarompo <icaromesquitaponce@outlook.com>
2023-12-08 14:40:24 -0300

    feat: cria carrousel

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Carousel from "../components/Carousel";
 
 export default function Home() {
     return (
-      <div >
-        <Header />
-          <Footer />
+        <div>
+            <Header />
+            <div className="container w-4/5 mt-16 mx-auto">
+                <Carousel/>
+            </div>
+            <Footer />
         </div>
     );
-  }
-  
+}
