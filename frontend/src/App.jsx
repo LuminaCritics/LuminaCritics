2445dcd2cae453ad00f82fcf2bd4adc41d286ba2
Bruno Ferreira da Silva <92815660+brunoF-Silva@users.noreply.github.com>
2023-12-08 11:30:40 -0300

    Estrutura do about.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
-
+import About from "./pages/About";
 export default function App() {
 
   return (
@@ -11,6 +11,7 @@ export default function App() {
         <Route path = "/" element = {<Home />}/>
         <Route path = "/login" element = {<Login />}/>
         <Route path = "/signup" element = {<SignUp />}/>
+        <Route path = "/about" element = {<About />}/>
       </Routes>
     </BrowserRouter>
   )
