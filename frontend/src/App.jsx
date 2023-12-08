e13158cb8e8d1c607816d1316de1bc421e17b953
Emanuel Montenegro <60626167+emanuelcatao@users.noreply.github.com>
2023-12-08 15:50:49 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
 import Contacts from "./pages/Contacts";
 import Privacy from "./pages/Privacy";
